0de5732ca1fa4ff9c313dcb032b13149d6f39ca4
Dio-ni <200103264@stu.sdu.edu.kz>
2024-03-20 00:47:53 +0600

    fixed app-changelog.yaml

--- src/main/resources/db/changelog/changes/V8_alter_saved_locations.sql
+++ src/main/resources/db/changelog/changes/V8_alter_saved_locations.sql
@@ -1,11 +1,1 @@
-drop table databasechangelog cascade;
-
-drop table databasechangeloglock cascade;
-
-drop table pin_verification cascade;
-
-drop table parent cascade;
-
-drop table child cascade;
-
-drop table app_user cascade;
+alter table saved_location ADD COLUMN radius INT default 25;
