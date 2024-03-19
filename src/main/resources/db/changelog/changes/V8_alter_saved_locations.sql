b792d79ff5b74b206ff52a981f16d8220c46b132
Dio-ni <200103264@stu.sdu.edu.kz>
2024-03-20 00:41:51 +0600

    exp 1

--- src/main/resources/db/changelog/changes/V8_alter_saved_locations.sql
+++ src/main/resources/db/changelog/changes/V8_alter_saved_locations.sql
@@ -1,1 +1,17 @@
-alter table saved_location ADD COLUMN radius INT default 25;
+drop table databasechangelog cascade;
+
+drop table databasechangeloglock cascade;
+
+drop table pin_verification cascade;
+
+drop table refresh_token cascade;
+
+drop table user_relationship cascade;
+
+drop table saved_location cascade;
+
+drop table child_location cascade;
+
+drop table hotline_number cascade;
+
+drop table app_user cascade;
