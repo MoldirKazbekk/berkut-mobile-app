58853dc8dbf4e827ab976d0b327fe9b1b08a52e3
Dio-ni <200103264@stu.sdu.edu.kz>
2024-03-20 00:45:04 +0600

    exp 2

--- src/main/resources/db/changelog/changes/V8_alter_saved_locations.sql
+++ src/main/resources/db/changelog/changes/V8_alter_saved_locations.sql
@@ -4,14 +4,8 @@ drop table databasechangeloglock cascade;
 
 drop table pin_verification cascade;
 
-drop table refresh_token cascade;
+drop table parent cascade;
 
-drop table user_relationship cascade;
-
-drop table saved_location cascade;
-
-drop table child_location cascade;
-
-drop table hotline_number cascade;
+drop table child cascade;
 
 drop table app_user cascade;
